2b7c55c5faeb5feab3db4315a232ab274891ef00
Yash Prasad <64632275+yashprasad234@users.noreply.github.com>
2024-04-10 12:37:34 +0530

    init

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config({ path: '../.env' });
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRouter = require("./routes/index");
+
+const app = express();
+
+const mongoConnectURL = process.env.DB_CONNECT_URL;
+
+// Allow requests from Vercel frontend
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api", userRouter);
+
+mongoose.connect(
+  `${mongoConnectURL}`,
+  {
+    dbName: "todoApp",
+  }
+);
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`);
+});
